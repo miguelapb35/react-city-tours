d4fe21c73ac44cf432df6626e22bbe75550b23ec
Miguel Angel <miguelapb35@gmail.com>
2020-05-15 14:47:50 -0500

    Updates

---
+++ precache-manifest.30bf37eed61cba350d614019440342a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08f76ac3c4f123d96d9ed32eb1896d06",
+    "url": "/react-city-tours/index.html"
+  },
+  {
+    "revision": "8d3e7cada75df984eaad",
+    "url": "/react-city-tours/static/css/main.d9ec0edc.chunk.css"
+  },
+  {
+    "revision": "577e794648d0d3a70753",
+    "url": "/react-city-tours/static/js/2.f47e2739.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-city-tours/static/js/2.f47e2739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d3e7cada75df984eaad",
+    "url": "/react-city-tours/static/js/main.746bc8e6.chunk.js"
+  },
+  {
+    "revision": "bc815bac9edc9f8579b4",
+    "url": "/react-city-tours/static/js/runtime-main.78f3572d.js"
+  }
+]);
